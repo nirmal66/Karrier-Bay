95d2d0fc2468c473c3acc858516445cd76c46a1f
nirmal <nirmal6060@gmail.com>
2017-01-28 13:02:52 +0530

    carrier list xml updated

---
+++ app/src/main/java/Model/User.java
@@ -0,0 +1,25 @@
+package Model;
+
+/**
+ * Created by vel on 24/1/17.
+ */
+
+
+
+
+
+public class User  {
+    private String firstName;
+
+    public User(String firstName) {
+        this.firstName = firstName;
+     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
