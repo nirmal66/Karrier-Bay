d46385407c10d32a52be5940b663dd9f5335cf9b
nirmal <nirmal6060@gmail.com>
2017-03-08 10:13:45 +0530

    defect fix for crash

--- app/src/main/java/RetroGit/ApiClient.java
+++ app/src/main/java/RetroGit/ApiClient.java
@@ -22,12 +22,12 @@ public class ApiClient {
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
 
-        if (retrofit == null) {
+//        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
-        }
+//        }
         return retrofit;
     }
    public static Retrofit getClientWithHeader(Context c) {
